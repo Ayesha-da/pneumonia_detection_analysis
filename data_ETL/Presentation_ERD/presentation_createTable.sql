d069ab836c7b590e8321c3c0d094e7d3236b36dd
nidhipandya <nidhipandya1987@gmail.com>
2021-11-05 22:27:16 -0400

    added ERD and schema for Presentation Database

---
+++ data_ETL/Presentation_ERD/presentation_createTable.sql
@@ -0,0 +1,59 @@
+Create Table Deaths_by_Month(
+month varchar,
+month_code date,
+deaths int);
+
+Create Table Deaths_by_Race(
+race int,
+race_code varchar PRIMARY KEY,
+deaths int,
+population int,
+crude_rate float);
+
+Create Table Deaths_by_State_by_Race(
+state varchar, 
+state_code int PRIMARY KEY,
+race int, 
+race_code varchar,
+deaths int,
+FOREIGN KEY(race_code) REFERENCES Deaths_by_Race(race_code)
+);
+
+Create Table Gender_Deaths(
+gender varchar,
+gender_code varchar PRIMARY KEY,
+deaths int,
+population int,
+crude_rate float);
+
+Create Table Deaths_by_State_by_Gender(
+state varchar,           
+state_code int,
+gender varchar,
+gender_code varchar,
+population int,
+crude_rate float,
+FOREIGN KEY(state_code) REFERENCES Deaths_by_State_by_Race(state_code) ,
+FOREIGN KEY(gender_code) REFERENCES Gender_Deaths(gender_code)
+);
+
+Create Table Deaths_by_Age_Group(
+five_year_age_groups varchar,
+five_year_age_groups_code int PRIMARY KEY,
+deaths int
+);
+
+
+Create Table Deaths_by_State_by_Age_Group(
+state varchar,
+state_code int,
+five_year_age_groups varchar,
+five_year_age_groups_code int,
+deaths int,
+FOREIGN KEY(state_code) REFERENCES Deaths_by_State_by_Race(state_code) ,
+FOREIGN KEY(five_year_age_groups_code) REFERENCES Deaths_by_Age_Group(five_year_age_groups_code)
+);
+
+
+
+
