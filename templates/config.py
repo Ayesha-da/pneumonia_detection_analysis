55f725ac7d55b15a56b4e3ec4365fefd8e40d13f
Ayesha <ayesha.school@hotmail.com>
2021-11-09 12:05:04 -0500

    Delete templates directory

--- templates/config.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Nov  6 07:57:12 2021
-
-@author: ayesh
-"""
-
-aws_access_key_id='AKIAW5XAT2GRZ2RTZ7GD',
-aws_secret_access_key= '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'
