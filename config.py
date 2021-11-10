1a8883aa5b73396602d70203f024784193236974
nidhipandya <nidhipandya1987@gmail.com>
2021-11-09 20:10:07 -0500

    added config file

---
+++ config.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov  5 10:48:13 2021
+
+@author: Nidhi
+"""
+from os import environ, path
+#from r-dotenv import load_dotenv
+
+
+basedir = path.abspath(path.dirname(__file__))
+#(path.join(basedir, '.env'))
+
+#FLASK_ENV = 'development'
+class Config:
+    ACCESS_KEY = environ.get('ACCESS_KEY')
+    SECRET_KEY = environ.get('SECRET_KEY')
